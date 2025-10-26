eaf1e4a10c7045df9a6a389c4acffb904316a88b
dahxxn <dahyeonhan4529@gmail.com>
2025-10-27 02:42:20 +0900

    feat(app): add Application main class for game initialization
    - Create and wire all dependencies (View, Service, Controller)
    - Initialize ForwardPolicy for game play logic
    - Execute game through RacingCarController

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import racingcar.controller.RacingCarController;
+import racingcar.policy.ForwardPolicy;
+import racingcar.service.GamePlayService;
+import racingcar.service.GameSetupService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        GameSetupService gameSetupService = new GameSetupService();
+        GamePlayService gamePlayService = new GamePlayService(new ForwardPolicy());
+        OutputView outputView = new OutputView();
+
+        RacingCarController racingCarController = new RacingCarController(inputView, gameSetupService, gamePlayService,
+                outputView);
+        racingCarController.run();
     }
 }
